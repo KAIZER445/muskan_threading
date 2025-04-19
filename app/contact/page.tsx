7ddcd88fd04fbaae34e3e92df9cf6ceb6ff545de
Prahlad-Ghimire <parhladghimire4545@gmail.com>
2025-04-19 21:04:33 +0545

    servicespagecompleted

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Contactlayone from '../contact_layouts/Contactlayone'
+import Contactlaytwo from '../contact_layouts/Contactlaytwo'
+import Contactlaythree from '../contact_layouts/Contactlaythree'
 
 
 const Contactpage = () => {
   return (
     <div>
      <Contactlayone />
+     <Contactlaytwo />
+     <Contactlaythree />
     </div>
   )
 }
