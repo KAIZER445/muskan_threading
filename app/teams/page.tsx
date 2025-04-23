6cee2635d1420cf33a98e00201dd604c39ccad30
Prahlad-Ghimire <parhladghimire4545@gmail.com>
2025-04-23 16:31:44 +0545

    ourteamspageupdating

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Teamlayoutone from '../teamlayouts/Teamlayoutone'
+import Teamlayouttwo from '../teamlayouts/Teamlayouttwo'
 
 const page = () => {
   return (
     <div>
       <Teamlayoutone />
+      <Teamlayouttwo />
     </div>
   )
 }
